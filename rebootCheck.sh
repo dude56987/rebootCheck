de902d42b77b5e8402bddf200831bc26dc76a8a9
dude56987 <dude56987@users.noreply.github.com>
2015-10-22 14:30:49 -0400

    Added option to set a flag file to activate reboot

--- rebootCheck.sh
+++ rebootCheck.sh
@@ -1,5 +1,17 @@
-if ping -c 1 $(cat /etc/rebootCheck.cfg);then
-	echo 'Still connected :D'
+check=$(cat /etc/rebootCheck.cfg)
+# if a webpages is used in config file
+if echo $check | grep -c "www.";then
+	if ping -c 1 $check;then
+		echo 'Still connected :D'
+	else
+		reboot
+	fi
 else
-	reboot
+	# if a file is give in config file
+	if [ -f $check ];then
+		rm $check
+		reboot
+	else
+		echo 'File flag does not exist ^_^'
+	fi
 fi
